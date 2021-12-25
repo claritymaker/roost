1785899ab7d8a5d838d2b3eccb919bef338e290c
Andrew Horvath <claritymaker@pm.me>
2021-12-24 22:54:36 -0500

    Moved CSV sink to its own file

---
+++ roost/csv_sink.py
@@ -0,0 +1,27 @@
+from typing import Dict
+
+from roost.storage import Storage
+
+
+class CSVSink:
+    def __init__(self, filename):
+        self.filename = filename
+        self.current_index = 0
+
+    def save(self, storage: Storage):
+        to_append = storage.pandas[self.current_index+1:]
+        if self.current_index == 0:
+            to_append.to_csv(self.filename)
+        else:
+            to_append.to_csv(self.filename, mode='a', header=False)
+
+        self.current_index = to_append.index[-1]
+
+
+class CSVStageSink:
+    def __init__(self, filename):
+        self.filename = filename
+        self.current_index = 0
+
+    def save(self, storage: Storage, key: str, packet: Dict[str, object]):
+        raise NotImplementedError("TODO")
